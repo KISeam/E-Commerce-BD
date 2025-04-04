223ca534ed9575ebc7e313386563f322e4df0dd3
Md. Seam <seam5559@gmail.com>
2025-04-04 11:00:16 +0600

    first-commit

--- src/main.jsx
+++ src/main.jsx
@@ -9,12 +9,12 @@ import Contact from "./Pages/Contact Us/Contact.jsx";
 import Blog from "./Pages/Blog/Blog.jsx";
 import AddToCarts from "./Pages/Add To Cart/AddToCarts.jsx";
 import FAQ from "./Pages/FAQ/FAQ.jsx";
-import Login from "./Components/Login/Login.jsx";
 import Products from "./Pages/Products/Products.jsx";
 import Wishlist from "./Pages/Wishlist/Wishlist.jsx";
-import Signup from "./Components/Signup/Signup.jsx";
 import SingleProduct from "./Components/SingleProduct.jsx";
 import SingleBlog from "./Components/SingleBlog.jsx";
+import Signup from "./Pages/Signup/Signup.jsx";
+import Login from "./Pages/Login/Login.jsx";
 
 const router = createBrowserRouter([
   {
