3e84de725ffcfee94eafc8bcfe67592c63f667ad
Md. Seam <seam5559@gmail.com>
2025-04-09 09:28:21 +0600

    first-commit

--- src/Root.jsx
+++ src/Root.jsx
@@ -4,13 +4,11 @@ import NavBar from "./Components/Header/NavBar";
 import TopHeader from "./Components/Header/TopHeader";
 import { Outlet } from "react-router-dom";
 import MidHeader from "./Components/Header/MidHeader";
-import MouseAnimation from "./Components/MouseAnimation";
 import ScrollButton from "./Components/ScrollButton";
 
 const Root = () => {
   return (
     <div className="bg-white">
-      <MouseAnimation />
       <TopHeader />
       <MidHeader />
       <NavBar />
