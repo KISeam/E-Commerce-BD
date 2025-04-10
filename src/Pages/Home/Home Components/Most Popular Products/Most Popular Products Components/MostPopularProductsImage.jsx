fcb527f94d95a9ce8196beccca6ac943007e6de6
Md. Seam <seam5559@gmail.com>
2025-04-10 22:14:39 +0600

    first commit

--- src/Pages/Home/Home Components/Most Popular Products/Most Popular Products Components/MostPopularProductsImage.jsx
+++ src/Pages/Home/Home Components/Most Popular Products/Most Popular Products Components/MostPopularProductsImage.jsx
@@ -12,12 +12,11 @@ const MostPopularProductsImage = () => {
         />
       </div>
 
-      <div className="absolute top-10 left-10 w-9/12">
+      <div className="absolute top-10 left-10 w-9/12 space-y-6">
         <h3 className="text-xl font-semibold text-gray-600">
           Our top most products check it now
         </h3>
-
-        <Link to="/products" className="px-4 py-2 mt-4 bg-[#5caf90] text-white rounded-lg text-sm">
+        <Link to="/products" className="px-4 py-2 bg-[#5caf90] text-white rounded-lg text-sm">
           Shop Now
         </Link>
       </div>
